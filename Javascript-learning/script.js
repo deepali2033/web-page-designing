4b3aa0d70752ecb4ae219671de2f17b68d3b66fd
deepali2033 <dptnwr2033@gmail.com>
2023-09-21 15:12:10 +0530

    changes

---
+++ Javascript-learning/script.js
@@ -0,0 +1,17 @@
+let js = "amazing";
+if (js === "amazing") alert("JavaScript is Fun!");
+// value and variable
+let country = "India";
+let continent = "Asia";
+let population = 1390;
+console.log(country);
+console.log(continent);
+console.log(population);
+
+// Data Type
+let isIsland = false;
+let language;
+console.log(typeof isIsland);
+console.log(typeof country);
+console.log(typeof population);
+console.log(typeof language);
