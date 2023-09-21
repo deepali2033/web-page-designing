678dd2d7f6d272976ca9a3743d341095c576b05f
deepali2033 <dptnwr2033@gmail.com>
2023-09-21 16:28:58 +0530

    changes

--- Javascript-learning/script.js
+++ Javascript-learning/script.js
@@ -1,17 +1,63 @@
 let js = "amazing";
 if (js === "amazing") alert("JavaScript is Fun!");
 // value and variable
-let country = "India";
-let continent = "Asia";
+
+//  let country = "India";
+//  let continent = "Asia";
 let population = 1390;
-console.log(country);
-console.log(continent);
+// console.log(country);
+// console.log(continent);
 console.log(population);
 
 // Data Type
-let isIsland = false;
+
+//  let isIsland = false;
 let language;
-console.log(typeof isIsland);
-console.log(typeof country);
-console.log(typeof population);
-console.log(typeof language);
+// console.log(typeof isIsland);
+// console.log(typeof country);
+// console.log(typeof population);
+// console.log(typeof language);
+
+// Let, const and var
+language = "Hindi";
+const country = "India";
+const continent = "Asia";
+const isIsland = false;
+
+console.log(country);
+console.log(continent);
+console.log(language);
+console.log(isIsland);
+
+// Basic Operators
+console.log(population / 2);
+population++;
+console.log(population);
+
+const FinlandPopulation = 6; // in million
+const averagePopulation = 33;
+console.log(FinlandPopulation < population);
+console.log(averagePopulation > population);
+
+const discription =
+  country +
+  "is in" +
+  "" +
+  continent +
+  ", and its" +
+  "" +
+  population +
+  "" +
+  "million people speak " +
+  language;
+console.log(discription);
+
+//Task
+const massMark = 78;
+const heighMark = 1.68;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+const BMIMark = massMark / (heightMark * heightMark);
+console.log(BMIJohn, BMIMark);
