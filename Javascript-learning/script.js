0d844cc3393d5ef817d28845c436141d18716c98
deepali2033 <dptnwr2033@gmail.com>
2023-09-22 21:15:04 +0530

    changes

--- Javascript-learning/script.js
+++ Javascript-learning/script.js
@@ -1,62 +1,72 @@
-let js = "amazing";
-if (js === "amazing") alert("JavaScript is Fun!");
-// value and variable
+// let js = "amazing";
+// if (js === "amazing") alert("JavaScript is Fun!");
+// // value and variable
 
-//  let country = "India";
-//  let continent = "Asia";
-let population = 1390;
+// //  let country = "India";
+// //  let continent = "Asia";
+// let population = 1390;
+// // console.log(country);
+// // console.log(continent);
+// console.log(population);
+
+// // Data Type
+
+// //  let isIsland = false;
+// let language;
+// // console.log(typeof isIsland);
+// // console.log(typeof country);
+// // console.log(typeof population);
+// // console.log(typeof language);
+
+// // Let, const and var
+// language = "Hindi";
+// const country = "India";
+// const continent = "Asia";
+// const isIsland = false;
+
 // console.log(country);
 // console.log(continent);
-console.log(population);
+// console.log(language);
+// console.log(isIsland);
 
-// Data Type
+// // Basic Operators
+// console.log(population / 2);
+// population++;
+// console.log(population);
 
-//  let isIsland = false;
-let language;
-// console.log(typeof isIsland);
-// console.log(typeof country);
-// console.log(typeof population);
-// console.log(typeof language);
+// const FinlandPopulation = 6; // in million
+// const averagePopulation = 33;
+// console.log(FinlandPopulation < population);
+// console.log(averagePopulation > population);
 
-// Let, const and var
-language = "Hindi";
-const country = "India";
-const continent = "Asia";
-const isIsland = false;
+// const description =
+//   country +
+//   " is in " +
+//   continent +
+//   " , and its " +
+//   population +
+//   " million people speak " +
+//   language;
+// console.log(discription);
 
-console.log(country);
-console.log(continent);
-console.log(language);
-console.log(isIsland);
+// //Task
+// const massMark = 78;
+// const heightMark = 1.68;
+// const massJohn = 92;
+// const heightJohn = 1.95;
 
-// Basic Operators
-console.log(population / 2);
-population++;
-console.log(population);
+// const BMIJohn = massJohn / (heightJohn * heightJohn);
+// const BMIMark = massMark / (heightMark * heightMark);
+// console.log(BMIJohn, BMIMark);
+// const markHigherBmi = BMIMark > BMIJohn;
+// console.log(markHigherBmi);
 
-const FinlandPopulation = 6; // in million
-const averagePopulation = 33;
-console.log(FinlandPopulation < population);
-console.log(averagePopulation > population);
+//Strings and Template Literals
 
-const discription =
-  country +
-  " is in " +
-  continent +
-  " , and its " +
-  population +
-  " million people speak " +
-  language;
-console.log(discription);
+let language = "Hindi";
+let country = "India";
+let continent = "Asia";
+let population = 1390;
 
-//Task
-const massMark = 78;
-const heightMark = 1.68;
-const massJohn = 92;
-const heightJohn = 1.95;
-
-const BMIJohn = massJohn / (heightJohn * heightJohn);
-const BMIMark = massMark / (heightMark * heightMark);
-console.log(BMIJohn, BMIMark);
-const markHigherBmi = BMIMark > BMIJohn;
-console.log(markHigherBmi);
+const description = ` ${country} is in ${continent}, and its ${population} million people speak ${language}`;
+console.log(description);
