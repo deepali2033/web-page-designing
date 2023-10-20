4f671a664dc9af2d6e046333251acc770dcc0dde
deepali2033 <dptnwr2033@gmail.com>
2023-10-20 23:19:36 +0530

    changes

--- Javascript-learning/developer-skills/script.js
+++ Javascript-learning/developer-skills/script.js
@@ -1,20 +1,20 @@
 "use strict";
-let score=20
-document.querySelector(".check").addEventListener(".click", function () {
-  const guess = Number( document.querySelector(".guess").value);
-  document.querySelector('.message').textContent='correct number'
-  console.log(document.querySelector('.message').textContent='correct number')
+let score = 20;
 
-  if( score===guess){
-    console.log(document.querySelector(".message").textContent)='Correct Number';
-  }
-  else if (!guess){
-    console.log(document.querySelector(".message").textContent)='No number';
-  }
-  else if (score<guess){
-    console.log(document.querySelector(".message").textContent)='Too high';
-  }
-  else if (score>guess){
-    console.log(document.querySelector(".message").textContent)='Too low';
-  }
+document.querySelector(".check").addEventListener(".click", function () {
+  document.querySelector(".message").textContent = "correct number";
+  console.log((document.querySelector(".message").textContent = "number"));
+
+  // if( score===guess){
+  //   console.log(document.querySelector(".message").textContent)='Correct Number';
+  // }
+  // else if (!guess){
+  //   console.log(document.querySelector(".message").textContent)='No number';
+  // }
+  // else if (score<guess){
+  //   console.log(document.querySelector(".message").textContent)='Too high';
+  // }
+  // else if (score>guess){
+  //   console.log(document.querySelector(".message").textContent)='Too low';
+  // }
 });
