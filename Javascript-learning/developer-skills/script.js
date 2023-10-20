a6ce2d3079eb3410ebefc9db112194af32f3744b
deepali2033 <dptnwr2033@gmail.com>
2023-10-20 22:38:06 +0530

    changes

---
+++ Javascript-learning/developer-skills/script.js
@@ -0,0 +1,20 @@
+"use strict";
+let score=20
+document.querySelector(".check").addEventListener(".click", function () {
+  const guess = Number( document.querySelector(".guess").value);
+  document.querySelector('.message').textContent='correct number'
+  console.log(document.querySelector('.message').textContent='correct number')
+
+  if( score===guess){
+    console.log(document.querySelector(".message").textContent)='Correct Number';
+  }
+  else if (!guess){
+    console.log(document.querySelector(".message").textContent)='No number';
+  }
+  else if (score<guess){
+    console.log(document.querySelector(".message").textContent)='Too high';
+  }
+  else if (score>guess){
+    console.log(document.querySelector(".message").textContent)='Too low';
+  }
+});
