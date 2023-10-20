89765acbea1dae3a4b7a9b088a1d8ecd4a068b5e
deepali2033 <dptnwr2033@gmail.com>
2023-10-21 00:23:30 +0530

    changes

--- Javascript-learning/developer-skills/script.js
+++ Javascript-learning/developer-skills/script.js
@@ -4,7 +4,8 @@ const secretNumber = Math.trunc(Math.random() * 20 + 1);
 document.querySelector(".number").textContent = secretNumber;
 
 document.querySelector(".check").addEventListener("click", function () {
-  const guess = document.querySelector(".guess").value;
+  const guess = parseInt(document.querySelector(".guess").value);
+  console.log("aaa", guess, secretNumber);
   if (!guess) {
     document.querySelector(".message").textContent = "No number";
   } else if (guess === secretNumber) {
