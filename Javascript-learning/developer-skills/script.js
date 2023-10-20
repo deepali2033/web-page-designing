ef7e287502a4b6a499f8ec54edf873a492589912
deepali2033 <dptnwr2033@gmail.com>
2023-10-21 00:45:17 +0530

    changes

--- Javascript-learning/developer-skills/script.js
+++ Javascript-learning/developer-skills/script.js
@@ -11,8 +11,20 @@ document.querySelector(".check").addEventListener("click", function () {
   } else if (guess === secretNumber) {
     document.querySelector(".message").textContent = "Correct Number ";
   } else if (guess < secretNumber) {
-    document.querySelector(".message").textContent = "Too Low ";
+    if (score > 0) {
+      document.querySelector(".message").textContent = "Too Low ";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "you lost the game! ";
+    }
   } else if (guess > secretNumber) {
-    document.querySelector(".message").textContent = "Too High ";
+    if (score > 1) {
+      document.querySelector(".message").textContent = "Too Low ";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "you lost the game! ";
+    }
   }
 });
