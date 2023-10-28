ad4fdade8bab0d494682d87d9f93ed26df88dccd
deepali2033 <dptnwr2033@gmail.com>
2023-10-28 17:34:40 +0530

    changes

--- Javascript-learning/Box-modal/script.js
+++ Javascript-learning/Box-modal/script.js
@@ -4,3 +4,5 @@ const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
 const btnsOpenModal = document.querySelectorAll(".show-modal");
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener("click", o);
