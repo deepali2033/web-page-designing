4fff55bba9abf003de594683a611ecc7c6676a08
deepali2033 <dptnwr2033@gmail.com>
2023-11-21 23:22:18 +0530

    changes

--- Javascript-learning/Hows-js-works/index.html/script.js
+++ Javascript-learning/Hows-js-works/index.html/script.js
@@ -1,1 +1,25 @@
 'use script'
+          /////----- Hosting-practice -----/////
+
+/*
+ //variable  
+
+console.log(me);
+console.log(job);      
+console.log(year);
+var me='jonas'
+ let job='student'
+const year=2000
+*/
+
+//Function
+console.log(addDecl(3,7))
+// console.log(addExpr(7,4))
+console.log(addArrow(3,4))
+function addDecl(a,b){
+    return a+b;
+}
+var addExpr=function(a,b){
+    return a+b;
+}
+var addArrow=(a,b)=>a+b;
