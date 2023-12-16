2bbb430e1f75b80c641f52e755203052a237bf1d
deepali2033 <dptnwr2033@gmail.com>
2023-12-16 17:53:09 +0530

    changes

--- Javascript-learning/fundamentals-js/script.js
+++ Javascript-learning/fundamentals-js/script.js
@@ -1150,3 +1150,25 @@ Afterwards, test with your own test data!;
 */
 
 
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+
+document.querySelector('button').addEventListener('click',function(){
+  const text =document.querySelector('textarea').value;
+  console.log(text);
+const rows =text.split('\n')
+console.log(rows)
+ 
+for(const row of rows){
+ const [first,second]= row.toLowerCase().trim().split('_')
+
+  console.log(first,second);
+  const output =`${first}${second.replace(
+    second[0],
+    second[0].toUpperCase()
+  )}`
+  console.log(output)
+}
+});
+
+
