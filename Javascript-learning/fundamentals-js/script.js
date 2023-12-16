f807b340e6e020d576f42cdccc69bb1e3a4e11b8
deepali2033 <dptnwr2033@gmail.com>
2023-12-16 18:46:39 +0530

    changes

--- Javascript-learning/fundamentals-js/script.js
+++ Javascript-learning/fundamentals-js/script.js
@@ -1172,3 +1172,14 @@ for(const [i, row] of rows.entries()){
 });
 
 */
+const flights =
+  `_Delayed_Departure;fao93766109;txl2133758440;11:25
+  +_Arrival;bru0943384722;fao93766109;11:45
+  +_Delayed_Arrival;hel7439299980;fao93766109;12:05
+  +_Departure;fao93766109;lis2323639855;12:30`;
+console.log(flights.split('+'))
+for (const flight of flights.split('+')){
+  const [type ,form, to,time]=flight.split(';');
+const output=`${type} ${from} ${to} (${time})`
+  console.log(flight.split(';'))
+}
