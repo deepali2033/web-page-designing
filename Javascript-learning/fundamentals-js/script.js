364231520a59ad5e8c96f26a1122a45cf3ab2adb
deepali2033 <dptnwr2033@gmail.com>
2023-12-18 23:28:09 +0530

    changes

--- Javascript-learning/fundamentals-js/script.js
+++ Javascript-learning/fundamentals-js/script.js
@@ -1183,7 +1183,7 @@ console.log(flights.split('+'))
 const getCode =str => str.slice(0,3).toUpperCase();
 for (const flight of flights.split('+')){
   const [type ,from, to,time]=flight.split(';');
-const output=`${type.startsWith('_Delayed') ?'🔴':' '} ${type.replaceAll('_',' ')} from ${getCode(from)}  to ${getCode(to)}  (${time.replace(":",'h')})`
+const output=`${type.startsWith('_Delayed') ?'🔴':''}${type.replaceAll('_',' ')} from ${getCode(from)} to ${getCode(to)} (${time.replace(":",'h')})`.padStart(36);
   // console.log(flight.split(';'))
 console.log(output)
 
