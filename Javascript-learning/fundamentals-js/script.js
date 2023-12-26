f4e229f1a11effaec8e0803ec81e520edc94bc9f
deepali2033 <dptnwr2033@gmail.com>
2023-12-26 21:59:14 +0530

    changes

--- Javascript-learning/fundamentals-js/script.js
+++ Javascript-learning/fundamentals-js/script.js
@@ -1258,6 +1258,6 @@ greetingHey('Rashi')
 greet('Hello')('Anjali')
 */
 // challenge 
-
+var x;
 const greetArr= greeting => name => console.log(`${greeting} ${name} what's up!`)
 greetArr('Hii')('Krishhh');
